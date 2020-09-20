629c3c449227a5f40700de1264e3ff322c3f6687
Ali Nguyen <alin57717@gmail.com>
2020-09-19 23:52:50 -0700

    add markdown for readme templater

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+# ${response.title}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+-
+
+
+
+## Description:
+
+${response.description}
+
+## Installation:
+
+${response.installation}
+
+## Usage:
+${response.usage}
+
+## Link:
+${response.link}
+
+##Contributors
+${response.contributors}
+
+##Languages
+${response.languages}
+
+##License
+${resposne.license}
+
+
 
 `;
 }
